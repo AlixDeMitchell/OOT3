5e2095abf5db9ec92aca3f8f911cf2f7e3564cc7
Alexandra DeMitchell <alix.demitchell@gmail.com>
2017-04-02 15:54:29 +0100

    Update Particle.h

--- Particle.h
+++ Particle.h
@@ -20,20 +20,32 @@ private:
 	//visible particle
 	SDL_Rect rect;
 
+	//iterator that tracks where each particls is within its owner bucket's list
 	std::list<std::shared_ptr<Particle>>::iterator m_iter;
 
 public:
 	//get and set which bucket the particle belongs to
 	std::weak_ptr<Bucket> GetOwner();
-	void SetOwner( std::weak_ptr<Bucket> _owner );
+	void SetOwner( std::weak_ptr<Bucket> _currentBucket );
 
+	//get and set iterator 
+	std::list<std::shared_ptr<Particle>>::iterator GetIter();
+	void SetIter( std::vector<std::shared_ptr<Bucket>> &_buckets );
+
+	//get position
+	glm::vec2 GetPosition();
+
+	//get SDL rect for drawing
 	SDL_Rect& GetRect();
 
 	//update
 	void Update();
 
 	//hash positions to indices
-	void Rehash( std::vector<std::shared_ptr<Bucket>> &_buckets );
+	/*void Rehash( std::vector<std::shared_ptr<Bucket>> &_buckets );
+*/
+	//check collision
+	bool CheckCollision( std::shared_ptr<Particle> _otherParticle );
 
 	Particle(double _x, double _y);
 	~Particle();
